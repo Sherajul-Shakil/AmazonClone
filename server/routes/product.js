26ee14051536da449f33d9ed46f089620de1dd2a
Sherajul-Shakil <sherajulshakil14@cse.pstu.ac.bd>
2022-06-06 22:36:35 +0600

    Search & show product in user side

--- server/routes/product.js
+++ server/routes/product.js
@@ -3,6 +3,7 @@ const productRouter = express.Router();
 const auth = require("../middlewares/auth");
 const { Product } = require("../models/product");
 
+//Fetch category product
 productRouter.get("/api/products/", auth, async (req, res) => {
   try {
     const products = await Product.find({ category: req.query.category });
@@ -12,5 +13,18 @@ productRouter.get("/api/products/", auth, async (req, res) => {
   }
 });
 
+
+// create a get request to search products and get them
+productRouter.get("/api/products/search/:name", auth, async (req, res) => {
+    try {
+      const products = await Product.find({
+        name: { $regex: req.params.name, $options: "i" },
+      });
+  
+      res.json(products);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  });
 
 module.exports = productRouter;
